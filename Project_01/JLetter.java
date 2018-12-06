f9b7bd1e3a51285081bf233edd92159d5a8e02cb
Natalie Eichorn <45643681+npeich@users.noreply.github.com>
2018-12-05 18:31:31 -0600

    Add files via upload

---
+++ Project_01/JLetter.java
@@ -0,0 +1,29 @@
+/**
+ * This program returns a "J" made out of "JAVA".
+ *
+ * Project 01.
+ * @author : Natalie Eichorn - COMP 1210 - 006.
+ * @version : 8/28/17.
+*/
+public class JLetter
+{
+   /**
+    * Prints each line with the correct number of
+    * J/A/V/A to make the J shape.
+    *
+    * @param args Command line arguments (not used).
+   */
+   public static void main(String[] args)
+   {
+      System.out.println("JAVAJAVAJAVA");
+      System.out.println("JAVAJAVAJAVA");
+      System.out.println("      JAVA");
+      System.out.println("      JAVA");
+      System.out.println("      JAVA");
+      System.out.println("      JAVA");
+      System.out.println("J     JAVA");
+      System.out.println("JA    JAVA");
+      System.out.println(" JAVAJAVA");
+      System.out.println("  JAVAJA");
+   }
+}
---
+++ Project_01/MyLifeGoals.java
@@ -0,0 +1,33 @@
+/**
+ * This program returns the life goals of Natalie.
+ *
+ * Project 01.
+ * @author : Natalie Eichorn - COMP 1210 - 006.
+ * @version : 8/28/17.
+*/
+public class MyLifeGoals
+{
+   /**
+   * Prints short term, medium term, and long term goals.
+   *
+   * @param args Command line arguments (not used).
+   */
+   public static void main(String[] args)
+   {
+      System.out.println("Natalie Eichorn");
+      System.out.println();
+      System.out.println("Short Term Goals : keep above a 3.5 GPA, "
+                              + "join 3 clubs, be able to run 3 miles straigt, "
+                              + "be a leader in my sorority, volunteer "
+                              + "when I can (ideally once a week).");
+      System.out.println("Medium Term Goals : get a great job at "
+                              + "a tech company, move to a cool city "
+                              + "(Austin/ATL), "
+                              + "get a dog, go to the Olympics in LA, "
+                              + "travel to every state in the US.");
+      System.out.println("Long Term Goals : have a happy and healthy family, "
+                              + "give back to society, have something "
+                              + "named after me, travel to every "
+                              + "continent (besides Antarctica).");
+   }
+}
